de6a77f46254b29a7982e77e220892b3ae55cf50
김민창 <rud0806@gmail.com>
2021-04-17 15:44:07 +0900

    first commit

---
+++ Mola/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mola
+//
+//  Created by 김민창 on 2021/04/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
