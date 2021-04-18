30ade520b1b3be772b58a2722710987ee545a37d
김민창 <rud0806@gmail.com>
2021-04-18 17:42:51 +0900

    adding id, password textfield

--- Mola/Controller/ViewController.swift
+++ Mola/Controller/ViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIGestureRecognizerDelegate {
+class ViewController: UIViewController, UIGestureRecognizerDelegate, UITextFieldDelegate {
     
     @IBOutlet weak var loginButtonLabel: UILabel!
     @IBOutlet weak var signUpButtonLabel: UILabel!
     @IBOutlet weak var idLabel: UILabel!
     @IBOutlet weak var passwordLabel: UILabel!
     @IBOutlet weak var forgotLabel: UILabel!
+    @IBOutlet weak var idText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    var tapIdTextField: Bool = false
+    var tapPasswordTextField: Bool = false
     
     @IBAction func loginTapView(_ sender: UITapGestureRecognizer) {
         print("login")
@@ -24,10 +29,39 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         self.navigationController?.pushViewController(singUpVC!, animated: true)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+    
+    @IBAction func idTapView(_ sender: UITapGestureRecognizer) {
+        self.idLabel.isHidden = true
+        self.idText.isHidden = false
+        self.tapIdTextField = true
+        self.idText.text = self.idLabel.text
+    }
+    
+    @IBAction func passwordTapView(_ sender: UITapGestureRecognizer) {
+        self.passwordLabel.isHidden = true
+        self.passwordText.isHidden = false
+        self.tapPasswordTextField = true
+        self.passwordText.text = self.passwordLabel.text
+    }
+    
+    func textFieldShouldReturn(_ Text: UITextField) -> Bool {
+        if tapIdTextField == true {
+            Text.resignFirstResponder()
+            self.idText.isHidden = true
+            self.idLabel.isHidden = false
+            tapIdTextField = false
+            self.idLabel.text = self.idText.text
+        } else if tapPasswordTextField == true {
+            Text.resignFirstResponder()
+            self.passwordText.isHidden = true
+            self.passwordLabel.isHidden = false
+            tapPasswordTextField = false
+            self.passwordLabel.text = self.passwordText.text
+        }
+        return true
+    }
+    
+    func initGesture() {
         let loginTapGesture: UITapGestureRecognizer =
             UITapGestureRecognizer(target: self, action: #selector(self.loginTapView(_:)))
         self.loginButtonLabel.isUserInteractionEnabled = true
@@ -37,6 +71,27 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
             UITapGestureRecognizer(target: self, action: #selector(self.signUpTapView(_:)))
         self.signUpButtonLabel.isUserInteractionEnabled = true
         self.signUpButtonLabel.addGestureRecognizer(signUpTapGesture)
+
+        self.idText.delegate = self
+        self.idText.isHidden = true
+        let idTapGesture = UITapGestureRecognizer(target: self, action: #selector(self.idTapView(_:)))
+        self.idLabel.isUserInteractionEnabled = true
+        idTapGesture.numberOfTapsRequired = 1
+        self.idLabel.addGestureRecognizer(idTapGesture)
+        
+        self.passwordText.delegate = self
+        self.passwordText.isHidden = true
+        let passwordTapGesture = UITapGestureRecognizer(target: self, action: #selector(self.passwordTapView(_:)))
+        self.passwordLabel.isUserInteractionEnabled = true
+        passwordTapGesture.numberOfTapsRequired = 1
+        self.passwordLabel.addGestureRecognizer(passwordTapGesture)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        initGesture()
     }
     
     override func viewWillAppear(_ animated: Bool) {
